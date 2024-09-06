16cc080fe139f3bab650865fcb6b382f6eaee8e9
Nico Haja <nhaja@MBP-von-Nico.fritz.box>
2024-09-06 20:03:45 +0200

    rename

--- scripts/update.py
+++ scripts/update.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-import helper.helper as helper
+import scripts.helper.file_helper as file_helper
 
 def update_vscode_settings():
     vscode_files = ['settings.json', 'keybindings.json']
-    vscode_sys_settings_path = helper.get_vscode_settings_path()
+    vscode_sys_settings_path = file_helper.get_vscode_settings_path()
     vscode_repo_settings_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), 
         os.pardir, 
@@ -18,11 +18,11 @@ def update_vscode_settings():
         print(f'\t- {file}', end='')
         src = os.path.join(vscode_sys_settings_path, file)
         dst = os.path.join(vscode_repo_settings_path, file)
-        helper.copy(src, dst)
+        file_helper.copy(src, dst)
         print(' [OK]')
 
 def update():
-    if helper.vscode_installed():
+    if file_helper.vscode_installed():
         update_vscode_settings()
 
 def main():
